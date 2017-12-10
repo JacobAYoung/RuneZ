c1f3bec0e10b17367397640eb419fa8056ce5bf9
Highvirus <yjake86@yahoo.com>
2017-12-09 21:54:39 -0600

    Updates
    Changed file name, also added a test image for background testing.

--- OSRS_Guides/ViewController.swift
+++ OSRS_Guides/ViewController.swift
@@ -13,8 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-      print("bruh")
-      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
