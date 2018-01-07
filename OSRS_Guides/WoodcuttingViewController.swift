c71d0982e26fd2300fa0057ee2074cc8c5f125c2
zjgt37 <31552774+zjgt37@users.noreply.github.com>
2018-01-07 16:51:35 -0600

    made scroll views and regular views on all skill view controllers added all buttons and text to skill view controllers named all buttons and associated text (this took way too long)

---
+++ OSRS_Guides/WoodcuttingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WoodcuttingViewController.swift
+//  OSRS_Guides
+//
+//  Created by Zach Galloway on 1/7/18.
+//  Copyright © 2018 Jake Young. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WoodcuttingViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setRunescapeBackButtonFont()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
